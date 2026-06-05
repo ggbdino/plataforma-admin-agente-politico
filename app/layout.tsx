4cb9fb467c6a8c26db997c9e310037d39199b4fc
Geraldino <ggbdino@gmail.com>
2026-06-05 19:50:11 -0300

    fix: v14.1.4 remove link da pagina publica de confirmacao do evento

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
     "Painel administrativo da GAP Consult Tecnologia para operação do agente político"
 };
 
-const APP_VERSION = "V.14.1.3";
+const APP_VERSION = "V.14.1.4";
 
 export default function RootLayout({
   children
