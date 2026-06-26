35fd76dd6a0b62efc4f3251e74d9a7003771758a
Geraldino <ggbdino@gmail.com>
2026-06-26 19:13:37 -0300

    fix: v16.0.4 tipa parametro booleano na atualizacao operacional

--- lib/version.ts
+++ lib/version.ts
@@ -1,9 +1,9 @@
-export const APP_VERSION = "V16.0.3";
+export const APP_VERSION = "V16.0.4";
 
 export const APP_VERSION_DETAIL = {
   major: 16,
   specification: 0,
-  hotfix: 3,
+  hotfix: 4,
   label: APP_VERSION
 } as const;
 
